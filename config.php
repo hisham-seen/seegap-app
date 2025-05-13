5b344fc2d6a944f866410a20579d66d5dd33ea97
Hisham Git Seen <hisham@seen.ie>
2025-05-13 23:10:18 +0100

    Fresh Copy

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   'localhost');
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', 'root');
-define('DATABASE_NAME',     'app_seegap');
-define('SITE_URL',          'http://localhost:8888/app.seegap.com/');
+define('DATABASE_SERVER',   '');
+define('DATABASE_USERNAME', '');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_NAME',     '');
+define('SITE_URL',          '');
