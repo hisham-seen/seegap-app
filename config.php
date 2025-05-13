386e2ce16593fc4af960663954d8ee29c9bea0e3
Hisham Git Seen <hisham@seen.ie>
2025-05-13 23:08:50 +0100

    Blank

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Configuration of the site */
+define('DATABASE_SERVER',   'localhost');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', 'root');
+define('DATABASE_NAME',     'app_seegap');
+define('SITE_URL',          'http://localhost:8888/app.seegap.com/');
