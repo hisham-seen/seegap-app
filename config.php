f7d9ea9edb5a20486333c070a91d6031d36399c8
Hisham Git Seen <hisham@seen.ie>
2025-06-15 11:43:08 +0100

    Remove .env and config.php from Git tracking - these files should be ignored

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Configuration of the site */
-define('DATABASE_SERVER',   'mysql');
-define('DATABASE_USERNAME', 'seegap_prod_user_2025');
-define('DATABASE_PASSWORD', 'SeeGap#Prod$2025!MySQL@Secure');
-define('DATABASE_NAME',     'seegap_production_db');
-define('SITE_URL',          'https://si.seegap.com/');
